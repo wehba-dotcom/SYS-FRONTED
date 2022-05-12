3fd2dd466afc50cf44848004777894a177a162e6
Rene <cph-ra147@cphbusiness.dk>
2022-05-12 21:49:57 +0200

    show all customer courses made in front

--- SEM3_EXAMEN_FRONTEND/src/components/MyPage.jsx
+++ SEM3_EXAMEN_FRONTEND/src/components/MyPage.jsx
@@ -1,7 +1,8 @@
 
-import React,{useEffect,useState} from 'react'
+import React, {useEffect,useState} from 'react'
 function MyPage() {
-    const [cc_course, setCourse]=useState([])
+    const [cc_course, setCourse] = useState([])
+
     const fetchData=async ()=>{
         const response =await fetch("http://localhost:8080/SYS_4SEM_war_exploded/api/customer/customercourses/1");
         const data = await response.json();
@@ -24,7 +25,7 @@ useEffect(()=>{
                 <th>Title</th>
                 <th>AnswersWrong</th>
                 <th>FinshDate</th>
-                <th>Customer</th>
+                <th>Customer id</th>
 
             </tr>
         </thead>
@@ -34,9 +35,8 @@ useEffect(()=>{
     {cc_course.map((course) => (<tr key={course.dto_cCID}>
         <td>{course.dto_cCID}</td>
         <td>{course.dto_title}</td>
-        <td>{course.dto_goal.answersWrong}</td>
-        <td>{course.dto_goal.finishDate}</td>
-    
+        <td>{course.dto_goal?.answersWrong}</td>
+        <td>{course.dto_goal?.finishDate}</td>
         <td>{course.dto_Customer.id}</td>
         
         
