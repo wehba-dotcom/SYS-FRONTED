7aa312cf4a57db5d76e6a17f12745fe5637b11b9
Rene <cph-ra147@cphbusiness.dk>
2022-05-17 10:17:13 +0200

    lavet set Goal on CC

--- SEM3_EXAMEN_FRONTEND/src/components/MyPage.jsx
+++ SEM3_EXAMEN_FRONTEND/src/components/MyPage.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import SetGoal from "./SetGoal";
+import "../styles/mypage.css";
+
 function MyPage() {
   const [cc_course, setCourse] = useState([]);
-  const [answers, setAnswers] = useState([]);
-  const [date, setDate] = useState([]);
+  const [goal, setGoal] = useState([]);
+  const [cc_id, setCcID] = useState("");
 
   const fetchData = async () => {
     const response = await fetch(
@@ -10,82 +13,85 @@ function MyPage() {
     );
     const data = await response.json();
     setCourse(data);
-    console.log(data);
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [goal]);
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
+  console.log(goal);
+  const setGoalOnCC = async (id, task) => {
+    const res = await fetch(
+      "http://localhost:8080/SYS_4SEM_war_exploded/api/customer/setgoal/" + id,
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      }
+    );
+    const data = res;
+    setGoal({ data });
   };
 
-  console.log("det her er date", date);
-  console.log("det her er answersWrong", answers);
-  const WrongAnswersNumbers = [0, 1, 2, 3, 4, 5];
   return (
     <div>
-      <p>All customer courses</p>
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Customer id</th>
-            <th>cc_Id/Title</th>
-            <th>AnswersWrong</th>
-            <th>FinshDate</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {cc_course.map((course) => (
-            <tr key={course.dto_cCID}>
-              <td>{course.dto_Customer.id}</td>
-              <td>
-                {course.dto_cCID} {course.dto_title}
-              </td>
-              <form onSubmit={onSubmit}></form>
-              {course.dto_goal?.answersWrong ? (
-                <td>{course.dto_goal?.answersWrong}</td>
-              ) : (
-                <td>
-                  {" "}
-                  <select onChange={(e) => setAnswers(e.target.value)}>
-                    {" "}
-                    <option>Select number</option>
-                    {WrongAnswersNumbers.map((c) => (
-                      <option key={c}>{c} </option>
-                    ))}{" "}
-                  </select>
-                </td>
-              )}
-              {course.dto_goal?.finishDate ? (
-                <td>{course.dto_goal?.finishDate}</td>
-              ) : (
-                <td>
-                  <input
-                    type="Date"
-                    onChange={(e) => setDate(e.target.value)}
-                  />
-                  {!course.dto_goal?.finishDate &&
-                    !course.dto_goal?.answersWrong && (
-                      <button
-                        style={{
-                          margin: 5,
-                          backgroundColor: "gray",
-                          border: 25,
-                          borderRadius: 5,
-                        }}
-                      >
-                        set goal
-                      </button>
-                    )}
-                </td>
-              )}
+      <div className="tablediv">
+        <p style={{ textAlign: "center" }}>All customer courses</p>
+        <table style={{ width: 750 }} className="table table-striped">
+          <thead>
+            <tr
+              style={{ backgroundColor: "darkgray" }}
+              className="myGoalTableContent"
+            >
+              {/* <th>Customer id</th> */}
+              {/* <th>cc_Id/Title</th> */}
+              <th>Title</th>
+              <th>Wrong Answers</th>
+              <th>Finsh Date</th>
+              <th>Set/Update</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="myGoalTableContent">
+            {cc_course.map((course) => (
+              <tr key={course.dto_cCID}>
+                {/* <td>{course.dto_Customer.id}</td> */}
+                {/* <td>
+                {course.dto_cCID} {course.dto_title}
+              </td> */}
+                <td className="border">
+                  <strong>{course.dto_title}</strong>
+                </td>
+
+                {course.dto_goal?.answersWrong ||
+                course.dto_goal?.answersWrong === 0 ? (
+                  <td className="border">{course.dto_goal?.answersWrong}</td>
+                ) : (
+                  <td className="border">You have not set a goal yet</td>
+                )}
+                {course.dto_goal?.finishDate ? (
+                  <td>{course.dto_goal?.finishDate}</td>
+                ) : (
+                  <td>You have not set a goal yet</td>
+                )}
+                <td className="border">
+                  {!course.dto_goal?.finishDate ||
+                  !course.dto_goal?.answersWrong ? (
+                    <SetGoal
+                      setccid={setCcID}
+                      setgoal={setGoalOnCC}
+                      getccid={course.dto_cCID}
+                    />
+                  ) : (
+                    <button>update</button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
