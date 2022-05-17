4560057b8f58d66223e5b3355a2e345bfb74cf2d
Rene <cph-ra147@cphbusiness.dk>
2022-05-17 11:41:52 +0200

    a little cleaning

--- SEM3_EXAMEN_FRONTEND/src/components/Header.jsx
+++ SEM3_EXAMEN_FRONTEND/src/components/Header.jsx
@@ -5,20 +5,9 @@ function Header() {
     <div style={{marginTop:'40px'}}>
       <ul className="header">
         <li> <NavLink exact activeClassName="active" to="/"> Home </NavLink></li>
-              
-      
   
           <li><NavLink activeClassName="active" to="/mypage">MyPage</NavLink></li>
-        
-     
-        
-       
          
-        
-       
-      
-        
-        
       </ul>
     </div>
   );
