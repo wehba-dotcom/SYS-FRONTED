d0fefd17f05651c41abf036f0042f30da7547035
cph-sn286 <73526072+cph-sn286@users.noreply.github.com>
2022-05-19 14:21:10 +0200

    made home

--- SEM3_EXAMEN_FRONTEND/src/components/Home.jsx
+++ SEM3_EXAMEN_FRONTEND/src/components/Home.jsx
@@ -4,13 +4,28 @@ function Home( ) {
   
   return (
     <div style={{textAlign:'center', margin:'20px',backgroundColor:'lightgreen'}}>
-      <h2>Welcome To This Travel Agency </h2>
+      <h2>Basic course - Set Goals </h2>
      
-      <p>In this  web application we have: </p>
+      
         <ul style={{ listStyleType: "none"}}>
-          <li>1-you can see all owers Trips</li>
-          <li>2-You can choise witch travel and add your self</li>
-          <li>3-You can see all guides in ower company</li>
+          <li>Primary actor: Customer
+              Secondary actor: System </li>
+              <br></br>
+          <li>precondition: customer is registered and signed in.</li>
+          <br></br>
+          <li style={{textAlign: "left"}}>
+     
+When the customer is on the "Home" page, the customer clicks on "My Page" in the navigation bar, the system forwards the customer to his/hers personal page. 
+
+On "My Page" the system gives the customer the opportunity to set goals for each of the courses with an active license. 
+  </li>
+  <br></br>
+
+    <li style={{textAlign: "left"}}>The customer can choose to set a "MAX questions answered wrong" by using a dropdown menu, and the 
+
+customer can choose to set a "date to be finished with the course" by using a calender. when the customer has entered the wished goals, the customer can press the "Set goal for this course " button. The system sets the goals in the database and display the goals in the Goal field for the given course.</li>
+
+
         </ul>
     </div>
   );
